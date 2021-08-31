e046235ce57468640f6d05d0d11559d927928212
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-08-31 15:06:34 +0200

    add tests with fixtures

--- pytestDemo/test_fixtureDemo.py
+++ pytestDemo/test_fixtureDemo.py
@@ -1,12 +1,17 @@
 import pytest
 
 
-@pytest.fixture()
-def setup():
-    print("I will be executing first")
-    yield
-    print("I will be executed last")
+@pytest.mark.usefixtures("setup")
+class TestExample:
 
+    def test_fixture_demo(self):
+        print("I will execute steps in fixture_demo method")
 
-def test_fixture_demo(setup):
-    print("I will execute steps in fixture_demo method")
+    def test_fixture_demo2(self):
+        print("I will execute steps in fixture_demo1 method")
+
+    def test_fixture_demo3(self):
+        print("I will execute steps in fixture_demo2 method")
+
+    def test_fixture_demo4(self):
+        print("I will execute steps in fixture_demo3 method")
