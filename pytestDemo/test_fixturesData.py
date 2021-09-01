19add024151449dad72eb234eb16a7f3a1f9e307
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-09-01 16:39:03 +0200

    add logs

--- pytestDemo/test_fixturesData.py
+++ pytestDemo/test_fixturesData.py
@@ -1,10 +1,14 @@
 import pytest
 
+from pytestDemo.BaseClass import BaseClass
+
 
 @pytest.mark.usefixtures("data_load")
-class TestExample2:
+class TestExample2(BaseClass):
 
     def test_edit_profile(self, data_load):
-        print(data_load[0])
-        print(data_load[1])
+        log = self.get_logger()
+        log.info(data_load[0])
+        log.info(data_load[2])
+        print(data_load[2])
 
