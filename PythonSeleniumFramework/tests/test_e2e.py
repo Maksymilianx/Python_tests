3a1f50fbc2910894275bb62657fdaba6f604de66
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-09-03 14:04:52 +0200

    add tests

---
+++ PythonSeleniumFramework/tests/test_e2e.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC, expected_conditions
+
+from PythonSeleniumFramework.utulities.BaseClass import BaseClass
+
+
+class TestOne(BaseClass):
+
+    def test_e2e(self):
+        self.driver.find_element_by_link_text("Shop").click()
+        products = self.driver.find_elements_by_xpath("//div[@class='card h-100']")
+        for product in products:
+            productName = product.find_element_by_xpath("div/h4/a").text
+            if productName == "Blackberry":
+                product.find_element_by_xpath("div/button").click()
+
+        self.driver.find_element_by_css_selector("a[class*='btn-primary']").click()
+        self.driver.find_element_by_xpath("//button[@class='btn btn-success']").click()
+        self.driver.find_element_by_id("country").send_keys("pol")
+        wait = WebDriverWait(self.driver, 7)
+        wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "Poland")))
+        self.driver.find_element_by_link_text("Poland").click()
+        self.driver.find_element_by_xpath("//div[@class='checkbox checkbox-primary']").click()
+        self.driver.find_element_by_css_selector("[type='submit']").click()
+        successText = self.driver.find_element_by_class_name("alert-success").text
+
+        assert "Success! Thank you!" in successText
